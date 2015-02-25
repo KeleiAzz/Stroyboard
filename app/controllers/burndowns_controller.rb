25892764dbb3ac5a73dac758f028946457449cdf
KeleiAzz <kgong@ncsu.edu>
2015-02-25 15:11:04 -0500

    align the two lines in burn down chart

--- app/controllers/burndowns_controller.rb
+++ app/controllers/burndowns_controller.rb
@@ -3,7 +3,7 @@ class BurndownsController < ApplicationController
   def show
     @dataArray = [10] * 10
 
-    @stories = Story.all.select{|s| s.project_id == params[:id].to_i}
+    @stories = Story.all.select{|s| s.project_id == params[:id].to_i && s.finishTime != nil}
     @stories.each do |story|
       for i in 1..story.finishTime do
         @dataArray[i-1] += story.point_value
